08c1f39af98db14d6df9d3fad0483784ce0fd020
Jenna Ramdenee <jenna_ramdenee@hotmail.co.uk>
2016-10-18 23:23:18 +0100

    Completed exercises

--- session1/challenge/6_string.rb
+++ session1/challenge/6_string.rb
@@ -1,11 +1,32 @@
 # You'll get a string and a boolean.
 # When the boolean is true, return a new string containing all the odd characters.
 # When the boolean is false, return a new string containing all the even characters.
-# 
+#
 # If you have no idea where to begin, remember to check out the cheatsheets for string and logic/control
-# 
+#
 # odds_and_evens("abcdefg",true)    # => "bdf"
 # odds_and_evens("abcdefg",false)   # => "aceg"
 
 def odds_and_evens(string, return_odds)
+  result = ""
+
+  if return_odds == true
+    #return string containing all odd chars
+    string.each_char.with_index do |letter, index|
+      if index.odd? == true
+        result = result + letter.to_s
+      end
+    end
+  end
+
+  if return_odds == false
+    #return string containing all even chars
+    string.each_char.with_index do |letter, index|
+      if index.even? == true
+        result = result + letter.to_s
+      end
+    end
+  end
+
+  return result
 end
--- session1/challenge/7_string.rb
+++ session1/challenge/7_string.rb
@@ -1,8 +1,18 @@
 # given a string, return the character after every letter "r"
-# 
+#
 # pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
 # pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+  result = ""
+  string.each_char.with_index do |letter, index|
+    unless string.size-1 == index
+      if (letter == "r" || letter == "R")
+        next_letter = string[index + 1]
+        result = result + next_letter
+      end
+    end
+  end
+  return result
 end
