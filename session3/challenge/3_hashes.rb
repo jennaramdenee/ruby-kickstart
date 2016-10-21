65786c6d67fd2ce174454155b008ad72f3fae01d
Jenna Ramdenee <jenna_ramdenee@hotmail.co.uk>
2016-10-21 11:19:27 +0100

    Complete

--- session3/challenge/3_hashes.rb
+++ session3/challenge/3_hashes.rb
@@ -6,3 +6,18 @@
 #
 # Example:
 # word_count "The dog and the cat" # => {"the" => 2, "dog" => 1, "and" => 1, "cat" => 1}
+
+def word_count(s)
+  if s == ""
+    return {}
+  end
+
+  result = {}
+  s.downcase!
+  s = s.split(" ")
+  s.each do |x|
+    x = x.to_s
+    result[x] = s.count(x)
+  end
+  return result
+end
