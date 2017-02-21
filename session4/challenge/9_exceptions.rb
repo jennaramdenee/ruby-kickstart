3c7825f63ab96584a453430f7f40210e33b6f3d2
jennaramdenee <jenna_ramdenee@hotmail.co.uk>
2017-02-21 22:09:12 +0000

    Completed

--- session4/challenge/9_exceptions.rb
+++ session4/challenge/9_exceptions.rb
@@ -1,16 +1,23 @@
 # You need to write a method that will take a person and
 # generate their full name. A full name is just a first_name
 # and a last_name separated by a space.
-# 
+#
 # Sometimes, though, the code will raise a FirstNameError
 # In these cases, just return their last name.
-# 
+#
 # Other times, the code will raise a LastNameError
 # In these cases, just return their first name.
-# 
+#
 # And sometimes, it will raise other unexpected errors.
 # In these cases, don't do anything, because we don't know
 # why that error was raised.
 
 def full_name(person)
+  begin
+    person.first_name + " " + person.last_name
+  rescue FirstNameError
+    return person.last_name
+  rescue LastNameError
+    return person.first_name
+  end
 end
