0d9a49d4e8b3da87e1f9b46117f898c9ae510c6c
jennaramdenee <jenna_ramdenee@hotmail.co.uk>
2017-02-21 18:45:08 +0000

    Complete

--- session4/challenge/4_oror.rb
+++ session4/challenge/4_oror.rb
@@ -1,6 +1,6 @@
 # Write a method first_object, that takes three parameters
 # and returns the first one that is true.
-# 
+#
 # EXAMPLES:
 # first_object 1, nil, nil    # => 1
 # first_object nil, 1, nil    # => 1
@@ -8,5 +8,9 @@
 # first_object nil, 1, 2      # => 1
 # first_object nil, nil, nil  # => nil
 
-def first_object
+def first_object(a, b, c)
+  [a,b,c].each do |elem|
+    return elem if elem != false && elem != nil
+  end
+  return nil
 end
